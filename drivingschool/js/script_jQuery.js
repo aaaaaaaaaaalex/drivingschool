fd19a1eebc84cf75149f998053698ae5be5011b5
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-06-11 07:17:57 +0400

    7:17 сделал нормальное вычитание высоты навигации при открытии/закрытии вложенных списков

--- drivingschool/js/script_jQuery.js
+++ drivingschool/js/script_jQuery.js
@@ -16,145 +16,69 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 	// Menu
-	// Menu Toggle
-	// let burger = document.querySelector(".burger");
-	// let nav = document.querySelector(".nav");
 
-	// let clickBurger = function(event) {
-	// 	event.preventDefault;
-	// 	burger.classList.toggle("active");
-	// 	nav.classList.toggle("active");
-
-	// };
-	// burger.addEventListener("click", clickBurger);
-
-	// Multilevel menu
+	// Vars
+	// Навигация
+	let nav = document.querySelector(".nav");
+	// Кнопка бургер
+	let burger = document.querySelector(".burger");
+	// Ссылки, при нажатии на которые должны открываться вложенные списки ul
 	let linksShowUl = document.querySelectorAll(".nav__link--show-ul");
+	// Вложенные списки ul
 	let ulsIn = document.querySelectorAll(".nav__ul-in");
 
+	// Изменение классов у нажатых ссылок+изменение высоты навигации при открытии/закрытии ul
 	let clickLinkShowUl = function(event) {
 		event.preventDefault;
+		// Ссылка, на которую нажал
 		let link = event.target;
-
+		// Уберу класс active у всех ссылок в цикле
 		for (let index = 0; index < linksShowUl.length; index++) {
 			const linkShowUl = linksShowUl[index];
 			linkShowUl.classList.remove("active");
 		}
-
-		// Найду навигацию
-		let navigation = document.querySelector(".nav");
-		// Сохраню высоту навигации
-		let heigtNavigation = navigation.clientHeight;
-		console.log("высота навигации до вычета высоты активного ul", heigtNavigation);
-
-		// Нахожу ul, у которого есть класс active
+		// ul, у которого есть класс active на момент нажатия ссылки до того, когда уберу у всех класс active
 		let ulInActive = document.querySelector(".nav__ul-in.active");
-		// console.log(ulInActive);
-		if(ulInActive == null) {
-			// Если такого ul нет, то
-			var heightUlInOld = 0;
-			console.log("нет ul c состоянием active");
-			console.log("установил высоту в значение",heightUlInOld);
-		}else{
-			// Если такой ul есть, то
-			// Сохраню высоту активного ul
-			var heightUlInOld = ulInActive.clientHeight;
-			console.log("есть ul c состоянием active");
-			console.log("высота ul у которого состояние active",heightUlInOld);
-
-			// Найду значение текущей высоты nav
-			// let heightNavCurrent = parseInt(nav.clientHeight, 10);
-			// console.log("высота nav до открытия ul",heightNavCurrent);
-			// nav.style.height = heightNavCurrent - heightUlInOld + "px";
-
+		if(ulInActive != null) {
+			// Tакой ul есть
 			// Вычту из высоты навигации высоту активного ul и запишу в высоту nav
-			navigation.style.height = heigtNavigation - heightUlInOld + "px";
-			console.log("высота навигации после вычитания высоты активного ul", navigation.style.height);
+			nav.style.height = parseInt(nav.style.height) - ulInActive.clientHeight + "px";
 		}
-
+		// Уберу класс active у всех ul в цикле
 		for (let index = 0; index < ulsIn.length; index++) {
 			const ulIn = ulsIn[index];
-
-
-
 			ulIn.classList.remove("active");
-			// console.log("ul, у которого убрали класс актив",ulIn);
-
-
-
-
-
-			// let heightNavOld = parseInt(nav.clientHeight, 10);
-			// nav.style.height = heightNavOld - heightUlIn + "px";
-			// console.log("высота nav до открытия ul",heightNavOld);
-			// console.log("новая высота nav",nav.style.height);
-
-			// if(heightUlInOld > 0) {
-			// 	var heightUlInOldLarge = heightUlInOld;
-			// }
-			// console.log(heightUlInOldLarge);
-
-
-
 		}
-		// console.log("цикл закончил работу");
-
+		// Добавлю класс active ссылке, на которую нажал
 		link.classList.add("active");
+		// Родительский li ссылки, на которую нажали
 		let parentLinkNavLi = link.closest(".nav__li");
+		// Родственный ul ссылки, на которую нажали
 		let ulIn = parentLinkNavLi.querySelector(".nav__ul-in");
-		console.log("родственный ul", ulIn);
-
+		// Добавлю класс active родственному ul ссылки, на которую нажали
 		ulIn.classList.add("active");
-
-		// Сохраню высоту открытого ul
-		let heightUlIn = ulIn.clientHeight;
-		console.log("высота открытого ul",heightUlIn);
-		// let heightNav = parseInt(nav.clientHeight, 10);
-		// nav.style.height = heightNav + heightUlIn + "px";
-
-		// Найду высоту nav после вычитания
-		let heightNavigationOld = navigation.clientHeight;
-		console.log("высота nav после вычитания", heightNavigationOld);
-
 		// Прибавлю к высоте навигации высоту активного ul
-		navigation.style.height = heightNavigationOld + heightUlIn + "px";
-
-
-		// console.log("высота nav до открытия ul",heightNav);
-		console.log("конечная высота nav после открытия ul", navigation.clientHeight);
-		console.log("конечная высота nav после открытия ul метод стайл", navigation.style.height);
-
-
+		nav.style.height = parseInt(nav.style.height) + ulIn.clientHeight + "px";
 	};
 	for (i=0; i<linksShowUl.length; i++) {
 		let linkShowUl = linksShowUl[i];
 		linkShowUl.addEventListener("click", clickLinkShowUl);
 	};
 
-
-
-
-
-	let nav = document.querySelector(".nav");
-	let burger = document.querySelector(".burger");
-
+	// Плавное Открытие/закрытие меню+aнимация бургера
 	let clickBurger = function (event) {
 		event.preventDefault();
-
+		burger.classList.toggle("active");
 		if (nav.classList.contains('active') == false) {
 			nav.classList.add('active');
 			nav.style.height = 'auto';
-
 			let height = nav.clientHeight + "px";
-
 			nav.style.height = '0px';
-
 			setTimeout(function () {
 				nav.style.height = height;
-			}, 100);
+			}, 200);
 		} else {
 			nav.style.height = '0px';
-
 			nav.addEventListener('transitionend', function () {
 				nav.classList.remove('active');
 			}, {
