24b3a8b0d0b219353f7e18616ffc79a49245c0fe
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-06-21 17:45:30 +0400

    17:45 сделал фиксированный хедер и прочеее

--- drivingschool/js/script_jQuery.js
+++ drivingschool/js/script_jQuery.js
@@ -27,14 +27,17 @@ document.addEventListener("DOMContentLoaded", function(){
 	let linksShowUl = document.querySelectorAll(".nav__link--show-ul");
 	// Вложенные списки ul
 	let ulsIn = document.querySelectorAll(".nav__ul-in");
+	// Создаем объект MediaQueryList
+	let mediaQueryList = window.matchMedia("(min-width: 1200px)");
 
 
+	// Function
 	// Функция, которая убирает класс active у всех ссылок, при нажатии на которые должны открываться вложенные списки ul
-	let removeActiveUlsIn = function() {
-		// Уберу класс active у всех вложенных ul в цикле
-		for (let index = 0; index < ulsIn.length; index++) {
-			const ulIn = ulsIn[index];
-			ulIn.classList.remove("active");
+	let removeActiveLinksShowUl = function() {
+		// Уберу класс active у всех ссылок в цикле
+		for (let index = 0; index < linksShowUl.length; index++) {
+			const linkShowUl = linksShowUl[index];
+			linkShowUl.classList.remove("active");
 		}
 	}
 	// Функция, которая убирает класс active у всех вложенных списков ul
@@ -45,41 +48,6 @@ document.addEventListener("DOMContentLoaded", function(){
 			ulIn.classList.remove("active");
 		}
 	}
-
-	// Изменение классов у нажатых ссылок+изменение высоты навигации при открытии/закрытии ul
-	let clickLinkShowUl = function(event) {
-		event.preventDefault;
-		// Ссылка, на которую нажал
-		let link = event.target;
-		// Уберу класс active у всех ссылок в цикле
-		for (let index = 0; index < linksShowUl.length; index++) {
-			const linkShowUl = linksShowUl[index];
-			linkShowUl.classList.remove("active");
-		}
-		// ul, у которого есть класс active на момент нажатия ссылки до того, когда уберу у всех класс active
-		let ulInActive = document.querySelector(".nav__ul-in.active");
-		if(ulInActive != null) {
-			// Tакой ul есть
-			// Вычту из высоты навигации высоту активного ul и запишу в высоту nav
-			nav.style.height = parseInt(nav.style.height) - ulInActive.clientHeight + "px";
-		}
-		removeActiveUlsIn();
-		// Добавлю класс active ссылке, на которую нажал
-		link.classList.add("active");
-		// Родительский li ссылки, на которую нажали
-		let parentLinkNavLi = link.closest(".nav__li");
-		// Родственный ul ссылки, на которую нажали
-		let ulIn = parentLinkNavLi.querySelector(".nav__ul-in");
-		// Добавлю класс active родственному ul ссылки, на которую нажали
-		ulIn.classList.add("active");
-		// Прибавлю к высоте навигации высоту активного ul
-		nav.style.height = parseInt(nav.style.height) + ulIn.clientHeight + "px";
-	};
-	for (i=0; i<linksShowUl.length; i++) {
-		let linkShowUl = linksShowUl[i];
-		linkShowUl.addEventListener("click", clickLinkShowUl);
-	};
-
 	// Функция рассчета высоты nav и записи вычесленного значения высоты в высоту nav
 	let heightNavCount = function() {
 		nav.style.height = 'auto';
@@ -89,6 +57,48 @@ document.addEventListener("DOMContentLoaded", function(){
 		setTimeout(function () {
 			nav.style.height = height;
 		}, 250);
+	};
+	// Функция, которая запускает функции убирание класса active и перерасчета высоты nav
+	let allFunction = function() {
+		removeActiveLinksShowUl();
+		removeActiveUlsIn();
+		heightNavCount();
+	}
+
+
+	// Изменение классов у нажатых ссылок+изменение высоты навигации при открытии/закрытии ul
+	let clickLinkShowUl = function(event) {
+		event.preventDefault;
+
+		if(mediaQueryList.matches == false) {
+			// Если ширина экрана маленькая, и навигация показывается в виде списка
+
+			// Ссылка, на которую нажал
+			let link = event.target;
+			removeActiveLinksShowUl();
+			// ul, у которого есть класс active на момент нажатия ссылки до того, когда уберу у всех класс active
+			let ulInActive = document.querySelector(".nav__ul-in.active");
+			if(ulInActive != null) {
+				// Tакой ul есть
+				// Вычту из высоты навигации высоту активного ul и запишу в высоту nav
+				nav.style.height = parseInt(nav.style.height) - ulInActive.clientHeight + "px";
+			}
+			removeActiveUlsIn();
+			// Добавлю класс active ссылке, на которую нажал
+			link.classList.add("active");
+			// Родительский li ссылки, на которую нажали
+			let parentLinkNavLi = link.closest(".nav__li");
+			// Родственный ul ссылки, на которую нажали
+			let ulIn = parentLinkNavLi.querySelector(".nav__ul-in");
+			// Добавлю класс active родственному ul ссылки, на которую нажали
+			ulIn.classList.add("active");
+			// Прибавлю к высоте навигации высоту активного ul
+			nav.style.height = parseInt(nav.style.height) + ulIn.clientHeight + "px";
+		}
+	};
+	for (i=0; i<linksShowUl.length; i++) {
+		let linkShowUl = linksShowUl[i];
+		linkShowUl.addEventListener("click", clickLinkShowUl);
 	};
 
 	// Плавное Открытие/закрытие меню+aнимация бургера
@@ -113,20 +123,44 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 	// Media
-	// Создаем объект MediaQueryList
-	let mediaQueryList = window.matchMedia("(min-width: 1200px)");
-
-	let handleWidthChange = function(event) {
-		if (event.matches) {
-			removeActiveUlsIn();
-			heightNavCount();
-		} else {
-			removeActiveUlsIn();
-			heightNavCount();
-		}
+	// Функция проверки изменения ширины экрана
+	let handleWidthChange = function() {
+		allFunction();
 	};
 	mediaQueryList.addListener(handleWidthChange);
+	handleWidthChange();
 
-	handleWidthChange(mediaQueryList);
+
+
+	// Header Fixed+
+	let header = document.querySelector(".header");
+	let logoWhite = document.querySelector(".logo__img--white");
+	let logoBlack = document.querySelector(".logo__img--black");
+	let burgerItem = document.querySelector(".burger__item");
+	let telWhite = document.querySelector(".tel--white");
+	let telBlack = document.querySelector(".tel--black");
+
+	let onScroll = function() {
+		if(window.pageYOffset > 100) {
+			header.classList.add("fixed");
+			logoWhite.classList.add("hidden");
+			logoBlack.classList.add("active");
+			burgerItem.classList.add("black");
+			telWhite.classList.add("hidden");
+			telBlack.classList.add("active");
+
+		} else {
+			header.classList.remove("fixed");
+			logoWhite.classList.remove("hidden");
+			logoBlack.classList.remove("active");
+			burgerItem.classList.remove("black");
+			telWhite.classList.remove("hidden");
+			telBlack.classList.remove("active");
+
+		}
+	};
+
+	window.addEventListener("scroll", onScroll);
+
 
 });
