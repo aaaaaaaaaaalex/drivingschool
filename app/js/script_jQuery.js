47125d0ad2cc80f6b22c3bcd6f8c99d4f2ab3987
aaaaaaaaaaalex <aleks.ru.mail.77@mail.ru>
2021-06-21 11:25:06 +0400

    11:25 работа над хэдером

--- app/js/script_jQuery.js
+++ app/js/script_jQuery.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	// Menu
 
+
 	// Vars
 	// Навигация
 	let nav = document.querySelector(".nav");
@@ -14,6 +15,24 @@ document.addEventListener("DOMContentLoaded", function(){
 	let linksShowUl = document.querySelectorAll(".nav__link--show-ul");
 	// Вложенные списки ul
 	let ulsIn = document.querySelectorAll(".nav__ul-in");
+
+
+	// Функция, которая убирает класс active у всех ссылок, при нажатии на которые должны открываться вложенные списки ul
+	let removeActiveUlsIn = function() {
+		// Уберу класс active у всех вложенных ul в цикле
+		for (let index = 0; index < ulsIn.length; index++) {
+			const ulIn = ulsIn[index];
+			ulIn.classList.remove("active");
+		}
+	}
+	// Функция, которая убирает класс active у всех вложенных списков ul
+	let removeActiveUlsIn = function() {
+		// Уберу класс active у всех вложенных ul в цикле
+		for (let index = 0; index < ulsIn.length; index++) {
+			const ulIn = ulsIn[index];
+			ulIn.classList.remove("active");
+		}
+	}
 
 	// Изменение классов у нажатых ссылок+изменение высоты навигации при открытии/закрытии ul
 	let clickLinkShowUl = function(event) {
@@ -32,11 +51,7 @@ document.addEventListener("DOMContentLoaded", function(){
 			// Вычту из высоты навигации высоту активного ul и запишу в высоту nav
 			nav.style.height = parseInt(nav.style.height) - ulInActive.clientHeight + "px";
 		}
-		// Уберу класс active у всех ul в цикле
-		for (let index = 0; index < ulsIn.length; index++) {
-			const ulIn = ulsIn[index];
-			ulIn.classList.remove("active");
-		}
+		removeActiveUlsIn();
 		// Добавлю класс active ссылке, на которую нажал
 		link.classList.add("active");
 		// Родительский li ссылки, на которую нажали
@@ -53,20 +68,28 @@ document.addEventListener("DOMContentLoaded", function(){
 		linkShowUl.addEventListener("click", clickLinkShowUl);
 	};
 
+	// Функция рассчета высоты nav и записи вычесленного значения высоты в высоту nav
+	let heightNavCount = function() {
+		nav.style.height = 'auto';
+		// Создам переменную height, в которую запишу вычисленную высоту после auto, а также прибавлю высоту псевдоэлемента в виде красной полоски снизу
+		let height = nav.clientHeight + 5 + "px";
+		nav.style.height = '0px';
+		setTimeout(function () {
+			nav.style.height = height;
+		}, 250);
+	};
+
 	// Плавное Открытие/закрытие меню+aнимация бургера
 	let clickBurger = function (event) {
 		event.preventDefault();
 		burger.classList.toggle("active");
 		if (nav.classList.contains('active') == false) {
 			nav.classList.add('active');
-			nav.style.height = 'auto';
-			let height = nav.clientHeight + "px";
-			nav.style.height = '0px';
-			setTimeout(function () {
-				nav.style.height = height;
-			}, 200);
+			heightNavCount();
 		} else {
 			nav.style.height = '0px';
+			console.log("установил высоту nav в 0");
+
 			nav.addEventListener('transitionend', function () {
 				nav.classList.remove('active');
 			}, {
@@ -75,5 +98,23 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	};
 	burger.addEventListener("click", clickBurger);
+
+
+	// Media
+	// Создаем объект MediaQueryList
+	let mediaQueryList = window.matchMedia("(min-width: 1200px)");
+
+	let handleWidthChange = function(event) {
+		if (event.matches) {
+			removeActiveUlsIn();
+			heightNavCount();
+		} else {
+			removeActiveUlsIn();
+			heightNavCount();
+		}
+	};
+	mediaQueryList.addListener(handleWidthChange);
+
+	handleWidthChange(mediaQueryList);
 
 });
